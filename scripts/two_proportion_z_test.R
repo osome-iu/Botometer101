783d316af37d73ff76341f87bc4a76731867c320
Kaicheng Yang <yangkc@iu.edu>
2022-05-14 21:23:46 -0400

    add: update z tests

--- scripts/two_proportion_z_test.R
+++ scripts/two_proportion_z_test.R
@@ -1,17 +1,17 @@
 ####################################
 # 0.5
 # shib vs. floki
-res <- prop.test(x = c(1215, 1130), n = c(2000, 2000))
+res <- prop.test(x = c(1098, 1061), n = c(1819, 1893))
 res$p.value
 sqrt(res$statistic)
 
 # shib vs. aapl
-res <- prop.test(x = c(1215, 1030), n = c(2000, 2000))
+res <- prop.test(x = c(1098, 936), n = c(1819, 1864))
 res$p.value
 sqrt(res$statistic)
 
 # floki vs. appl
-res <- prop.test(x = c(1130, 1030), n = c(2000, 2000))
+res <- prop.test(x = c(1061, 936), n = c(1893, 1864))
 res$p.value
 sqrt(res$statistic)
 
@@ -19,16 +19,16 @@ sqrt(res$statistic)
 ####################################
 # 0.7
 # shib vs. floki
-res <- prop.test(x = c(591, 578), n = c(2000, 2000))
+res <- prop.test(x = c(524, 557), n = c(1819, 1893))
 res$p.value
 sqrt(res$statistic)
 
 # shib vs. aapl
-res <- prop.test(x = c(591, 760), n = c(2000, 2000))
+res <- prop.test(x = c(524, 688), n = c(1819, 1864))
 res$p.value
 sqrt(res$statistic)
 
 # floki vs. appl
-res <- prop.test(x = c(578, 760), n = c(2000, 2000))
+res <- prop.test(x = c(557, 688), n = c(1893, 1864))
 res$p.value
 sqrt(res$statistic)
